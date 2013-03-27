1c8c1041a8825956168ada778b493df3d3908673
tailang <caopingcpu@163.com>
2013-03-28 00:15:42 +0800

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,54 +5,60 @@ describe "Static pages" do
   describe "Home page" do
 
   	it "should have the content 'Sample App'" do
-  		visit '/static_pages/home'
+      visit root_path
   		page.should have_content('Sample App')
   	end
 
-  	# it "should have the right title" do
-  	# 	visit '/static_pages/home'
-  	# 	page.should have_selector('title',
-  	# 						:text => "Ruby on Rails Tutorial Sample App")
-  	# end
+  	it "should have the right title" do
+      visit root_path
+  		page.should have_selector('title',
+  							:text => "Ruby on Rails Tutorial Sample App | Home")
+  	end
   end
 
 
   describe "Help page" do
 
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+      visit help_path
   		page.should have_content('Help')
   	end
-# it "should have the right title" do
-#   		visit '/static_pages/help'
-#   		page.should have_selector('title',
-#   							:text => "Ruby on Rails Tutorial Sample App|Help")  	
-#   end
+it "should have the right title" do
+      visit help_path
+  		page.should have_selector('title',
+  							:text => "Ruby on Rails Tutorial Sample App | Help")  	
+  end
 
-end
+  end
 
   describe "About page" do
 
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
+      visit about_path
   		page.should have_content('About Us')
   	end
 
-# it "should have the right title" do
-#   		visit '/static_pages/about'
-#   		page.should have_selector('title',
-#   							:text => "Ruby on Rails Tutorial Sample App|About Us")
-#   end
+it "should have the right title" do
+       visit about_path     
+  		page.should have_selector('title',
+  							:text => "Ruby on Rails Tutorial Sample App | About Us")
+  end
+
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 
 end
-end
 
-# describe "StaticPages" do
-#   describe "GET /static_pages" do
-#     it "works! (now write some real specs)" do
-#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#       get static_pages_index_path
-#       response.status.should be(200)
-#     end
-#   end
-# end
