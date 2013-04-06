62a2e0c4a0b1b22883c183907d3297253e078038
tailang <caopingcpu@163.com>
2013-04-06 15:24:00 +0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
  end
   
  def new
+ 	@user = User.new
+ end
+
+ def create
+	@user = User.new(params[:user])
+	#提交表单后，rails会构建一个名为user的Hash，其键值是imput标签的那么属性值，
+	#键对应的值是用户填写的字段文本。等同与
+	#@user = User.new(name: "xxx",email: "xxx",
+	#	              password: "xxx",password confirmation: "xxx")
+	if @user.save
+		flash[:success] = "welcome to the sample app"
+		redirect_to @user
+
+	else
+	  render 'new'
+	end
  end
 end
+
